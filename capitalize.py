ba7e398fe056de333bb4952f3e89b0e2707b7f9c
Lanca922 <pawel.922r@gmail.com>
2022-05-22 15:50:44 +0200

    create two files

---
+++ capitalize.py
@@ -0,0 +1,33 @@
+import math
+imie = "arkadiusz"
+#213
+print(imie.capitalize()) #Wypisuje stringa z dużej litery NIE ZMIENIA ZMIENNEJ imie
+
+imie = imie.capitalize() # Zmienia 1 literę zmiennej imie na stałe
+
+print(imie)
+
+imie = imie.upper()
+print(imie)
+
+imie = imie.lower()
+print(imie)
+
+print(math.sqrt(4))
+
+a, b = 2, 3
+
+print(a)
+print(b)
+imie1, imie2, imie3, nazw1, nazw2, nazw3 = "pawel", "pawel", "pawel", "pawel", "pawel", "pawel"
+
+print(imie1, imie2)
+
+imie1 = "Paweł"
+imie2 = "Rutkowki"
+
+print(imie1, imie2)
+
+print(imie1 + imie2)
+
+print(imie + " " + imie2)
